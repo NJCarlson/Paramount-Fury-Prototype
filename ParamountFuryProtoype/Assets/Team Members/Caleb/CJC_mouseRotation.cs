392b0ddf7c1232bffd7e2ecbff4b75fb15703e31
DESKTOP-E1TJKOT\caleb christerson <christersoncj@yahoo.com>
2017-10-10 12:26:09 -0400

    character rotation
    did the thing

---
+++ ParamountFuryProtoype/Assets/Team Members/Caleb/CJC_mouseRotation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CJC_mouseRotation : MonoBehaviour {
+
+
+	void Update () 
+	{
+		//Get the Screen positions of the object
+		Vector2 positionOnScreen = Camera.main.WorldToViewportPoint (transform.position);
+
+		//Get the Screen position of the mouse
+		Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+		//Get the angle between the points
+		float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+
+		//Ta Daaa
+		transform.rotation =  Quaternion.Euler (new Vector3(0f,0f,angle));
+	}
+
+	float AngleBetweenTwoPoints(Vector3 a, Vector3 b) {
+		return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+	}
+
+}
