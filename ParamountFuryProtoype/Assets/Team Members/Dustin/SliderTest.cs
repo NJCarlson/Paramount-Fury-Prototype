aa709cc4ade50b09cf2757f0509c7ad185955284
Dthejust <32401334+Dthejust@users.noreply.github.com>
2017-10-11 12:40:42 -0400

    Add files via upload

---
+++ ParamountFuryProtoype/Assets/Team Members/Dustin/SliderTest.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliderTest : MonoBehaviour {
+
+	public void OnValueChanged(float value){
+		Debug.Log ("New Value" + value);
+	}
+}
