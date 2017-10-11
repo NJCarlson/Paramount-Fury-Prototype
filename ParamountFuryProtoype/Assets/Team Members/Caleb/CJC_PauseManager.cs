de8b6311fd5cab41a0d7ee4ded85b3db071d2ee9
DESKTOP-E1TJKOT\caleb christerson <christersoncj@yahoo.com>
2017-10-10 20:40:15 -0400

    pause shit
    yep it's shit

---
+++ ParamountFuryProtoype/Assets/Team Members/Caleb/CJC_PauseManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CJC_PauseManager : MonoBehaviour {
+
+	public static bool gamepaused;
+
+	[SerializeField]
+	GameObject pausedOBJ;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		controlpuased ();
+		pausecontrols ();
+	}
+
+	void pausecontrols()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape) | Input.GetKeyDown (KeyCode.P) | Input.GetButtonDown ("360_StartButton"))
+		{
+			if (gamepaused)
+			{
+				gamepaused = false;
+				pausedOBJ.SetActive (false);
+			}
+			else if (!gamepaused)
+			{
+				gamepaused = true;
+				pausedOBJ.SetActive (true);
+			}
+		}
+	}
+
+	void controlpuased()
+	{
+		if (gamepaused) 
+		{
+			Time.timeScale = 0;
+		}
+		else if (!gamepaused) 
+		{
+			Time.timeScale = 1;
+		}
+	}
+}
