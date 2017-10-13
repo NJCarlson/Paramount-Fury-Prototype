af498e85caedb0b4315c5a0c3bbaa02263f815ac
DESKTOP-E1TJKOT\caleb christerson <christersoncj@yahoo.com>
2017-10-13 17:49:49 -0400

    asdfasdfa

--- ParamountFuryProtoype/Assets/Team Members/Caleb/CJC_scripts/CJC_CollectibleItem.cs
+++ ParamountFuryProtoype/Assets/Team Members/Caleb/CJC_scripts/CJC_CollectibleItem.cs
@@ -4,20 +4,47 @@ using UnityEngine;
 
 public class CJC_CollectibleItem : MonoBehaviour {
 
+	float timer;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
+
+
 		transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+		GameObject jerrbear = GameObject.FindWithTag ("GameController");
+		CJC_MissionControl jerry = jerrbear.GetComponent<CJC_MissionControl> ();
+
+
+		if (jerry.missionName != ("Prized Posessions"))
+			{
+				timer += Time.deltaTime;
+			}
+
+		if (timer > 1)
+		{
+			
+			Destroy (gameObject, 0);
+		}
+
 	}
 
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.tag == "Player")
 		{
+			GameObject jerrbear = GameObject.FindWithTag ("GameController");
+			CJC_MissionControl jerry = jerrbear.GetComponent<CJC_MissionControl> ();
+
+			if (jerry.missionName == ("Prized Posessions"))
+			{
+				jerry.PrizedCollectiblesCUR++;
+			}
 			Destroy (gameObject);
 		}
 	}
