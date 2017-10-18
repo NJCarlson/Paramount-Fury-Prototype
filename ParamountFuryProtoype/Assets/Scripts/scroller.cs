d127e5df5855b8bacfdff4216a861d66d2620a3d
dizzyA <TSRoss@fullsail.edu>
2017-10-18 15:03:21 -0400

    intro scene

---
+++ ParamountFuryProtoype/Assets/Scripts/scroller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scroller : MonoBehaviour {
+
+    float timer = 45f;
+
+	// Update is called once per frame
+	void Update ()
+	{
+        transform.position += transform.up * Time.deltaTime * 40;
+        if (Input.anyKey || timer <= 0)
+        {
+            SceneManager.LoadScene("Tutorial");
+        }
+        timer -= Time.deltaTime;
+        print(timer);
+    }
+}
